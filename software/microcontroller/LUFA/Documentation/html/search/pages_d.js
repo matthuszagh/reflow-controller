218c2694096d13ba499922b6e509619d4e819138
matthuszagh <huszaghmatt@gmail.com>
2018-07-25 11:10:07 -0400

    Updated microcontroller code.

---
+++ software/microcontroller/LUFA/Documentation/html/search/pages_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['the_20lufa_20build_20system',['The LUFA Build System',['../_page__build_system.html',1,'Page_DevelopingWithLUFA']]],
+  ['troubleshooting_20information',['Troubleshooting Information',['../_page__build_troubleshooting.html',1,'Page_BuildSystem']]]
+];
