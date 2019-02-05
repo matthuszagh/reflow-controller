906a41cb823be7fc73dcb93dcbdd46e1053912ff
Matt Huszagh <huszaghmatt@gmail.com>
2019-02-05 11:57:01 -0800

    Improved mcu and pc code.

--- software/mcu/reflow_future.h
+++ software/mcu/reflow_future.h
@@ -1,5 +1,6 @@
 #ifndef _REFLOW_H
 #define _REFLOW_H
+
 #include <avr/eeprom.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
@@ -14,10 +15,10 @@
 #include <LUFA/Drivers/Peripheral/Serial.h>
 #include <LUFA/Drivers/USB/USB.h>
 
-#define _NOP()                                                                                     \
-	do {                                                                                       \
-		__asm__ __volatile__("nop");                                                       \
-	} while (0)
+#define _NOP()                                                                 \
+  do {                                                                         \
+    __asm__ __volatile__("nop");                                               \
+  } while (0)
 #define bit_set(p, m) ((p) |= (1 << m))
 #define bit_clear(p, m) ((p) &= ~(1 << m))
 #define _ICR1 6250 /* Counts 0.2 seconds */
@@ -31,25 +32,30 @@ void EVENT_USB_Device_Disconnect(void);
 void EVENT_USB_Device_ConfigurationChanged(void);
 void EVENT_USB_Device_ControlRequest(void);
 
-/* Temperatures in quarter celsius. Time in seconds. */
 typedef struct {
-	uint16_t start_rate;
-	uint16_t soak_temp1;
-	uint16_t soak_temp2;
-	uint16_t soak_length;
-	uint16_t peak_temp;
-	uint16_t time_to_peak;
-	uint16_t cool_rate;
-	uint16_t pid_p;
-	uint16_t pid_i;
-	uint16_t pid_d;
+  uint16_t preheat_rate;
+  uint16_t soak_temp1;
+  uint16_t soak_temp2;
+  uint16_t soak_time;
+  uint16_t liquides_temp;
+  uint16_t time_to_liquides;
+  uint16_t peak_temp;
+  uint16_t time_to_peak;
+  uint16_t cool_rate;
+  uint16_t pid_p;
+  uint16_t pid_i;
+  uint16_t pid_d;
 } profile_t;
 
-typedef enum { T_STOP = 0, T_START = 1, T_SOAK = 2, T_PEAK = 3, T_COOL = 4 } state_enum;
+typedef enum { STOP = 0, PREHEAT, SOAK, REFLOW_RAMP, REFLOW_TAL, COOL } state;
 
-uint16_t target_temp(uint16_t temp, uint16_t *timer);
+uint16_t get_target(uint16_t temp, uint16_t *timer);
 uint16_t pid(uint16_t, uint16_t);
 void set_profile(void);
 void write_profile(void);
 void output_profile(void);
+void setup_hardware(void);
+uint16_t read_temp(void);
+void usb_rx(void);
+
 #endif
